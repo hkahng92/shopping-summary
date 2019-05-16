247a7045a99b1c5696de7036afab8e08e2522d94
hkahng92@gmail.com <hkahng92@gmail.com>
2019-05-16 19:40:58 -0400

    Updates

---
+++ precache-manifest.56031827d88d363bfda0c684ae70b9f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9be3ae4c91b7797ff5fc9038f76a612",
+    "url": "/shopping-summary/index.html"
+  },
+  {
+    "revision": "b6855e62b6578f35b181",
+    "url": "/shopping-summary/static/css/main.7f0b972d.chunk.css"
+  },
+  {
+    "revision": "9965a3d06b0f9b041e19",
+    "url": "/shopping-summary/static/js/2.661d8a27.chunk.js"
+  },
+  {
+    "revision": "b6855e62b6578f35b181",
+    "url": "/shopping-summary/static/js/main.7359a0e1.chunk.js"
+  },
+  {
+    "revision": "043131627f9ee91296aa",
+    "url": "/shopping-summary/static/js/runtime~main.62ecc992.js"
+  }
+]);
